1a4b49f2e82df798029bbe87437c79404219f0f1
cc <xiaojucc740@gmail.com>
2020-02-26 21:06:58 -0800

    Set up for Pass writing

---
+++ Pass/build/CMakeFiles/LLVMLivenessAnalysis.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/cc/Desktop/HelloPass-LLVM-master/Pass/Transforms/LivenessAnalysis/LivenessAnalysis.cpp" "/Users/cc/Desktop/HelloPass-LLVM-master/Pass/build/CMakeFiles/LLVMLivenessAnalysis.dir/LivenessAnalysis.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LLVMLivenessAnalysis_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/cc/Desktop/ucr/2020/WINTER/CS201/project/llvm/include"
+  "/Users/cc/Desktop/ucr/2020/WINTER/CS201/project/build/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Pass/build/CMakeFiles/LLVMLivenessAnalysis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LLVMLivenessAnalysis.dir/LivenessAnalysis.cpp.o"
+  "libLLVMLivenessAnalysis.pdb"
+  "libLLVMLivenessAnalysis.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/LLVMLivenessAnalysis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
