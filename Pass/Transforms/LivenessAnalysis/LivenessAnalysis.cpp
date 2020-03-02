ffb53503185a21be3260810df74f659cd01fcfaa
cc <xiaojucc740@gmail.com>
2020-03-01 22:48:19 -0800

    explain code and change print

--- Pass/Transforms/LivenessAnalysis/LivenessAnalysis.cpp
+++ Pass/Transforms/LivenessAnalysis/LivenessAnalysis.cpp
@@ -26,13 +26,14 @@ struct Liveness : public FunctionPass {
   Liveness() : FunctionPass(ID) {
 //      UEE = new std::map<BasicBlock*, std::vector<std::string>*>;
   }
-
+    // LIVEOUT(b) = Union_(succ(b) as x) (LIVEOUT (x) - VARKILL (x) + UEEXPR (x)) 
     std::map<BasicBlock*, std::vector<std::string>*> killSet;
     std::map<BasicBlock*, std::vector<std::string>*> UEE;
     std::map<BasicBlock*, std::vector<std::string>*> liveOut;
     std::list<BasicBlock*> worklist;
 
-
+    // TODO: using .ll file, null name, use getOperand() 
+    // get name of each variable
     std::string *getValueName(Use *it){
         std::string *op = new std::string;
         if ((*it)->hasName()){
@@ -45,6 +46,13 @@ struct Liveness : public FunctionPass {
     }
 
     // Parse the block from top to down, Add operands not existed in the KillSet and Add destination to the kill set
+    // FOR i = 1 to k
+    // assume oi is “x = y op z”
+    // IF y not in VARKILL(n)
+    // UEVar(n) = UEVAR(n) + y
+    // IF z not in VARKILL(n)
+    // UEVar(n) = UEVAR(n) + z
+    // VARKILL(n) = VARKILL(n) + x
     void UEVar(BasicBlock* bb){
         std::pair<BasicBlock*,std::vector<std::string>*> *temp = new std::pair<BasicBlock*,std::vector<std::string>*>;
         auto killSet = new std::vector<std::string>;
@@ -52,28 +60,34 @@ struct Liveness : public FunctionPass {
         auto UEE = new std::vector<std::string>;
         std::vector<std::string>::iterator killSetIt;
         std::vector<std::string>::iterator UEEIt;
+        //TODO: name of basic block, need to store and output
         errs() << bb->getName() << ":\n";
+        errs() << "BB:\n";
         for (auto& inst : *bb)
         {
-            errs() << "\t"  << inst << "\n";
+            // errs() << "\t"  << inst << "\n";
           if(inst.getOpcode() == Instruction::Load){
-            errs() << "This is Load"<<"\n";
+            // errs() << "This is Load"<<"\n";
           }
           if(inst.getOpcode() == Instruction::Store){
-            errs() << "This is Store"<<"\n";
+            // errs() << "This is Store"<<"\n";
           }
+          //if operator is assignment or phi or jump
           if (inst.isBinaryOp() || inst.getOpcode()==Instruction::PHI || inst.getOpcode()==Instruction::ICmp)
           {
               //errs() << "inst:\t"  << inst <<  " is binary" << "\n";
             auto* ptr = dyn_cast<User>(&inst);
             auto* op3 = dyn_cast<Value>(&inst);
             //errs() << "\t" << *ptr << "\n";
+            //TODO: change getValueneme function to getoperand()
             int i = 1;
             for (auto it = ptr->op_begin(); it != ptr->op_end(); ++it) {
                 auto var = getValueName(it);
                 if(var!=NULL){
+                    // if the variable is not in KILLSET
                     if(std::find(killSet->begin(), killSet->end(), *var) == killSet->end()) {
-//                        errs() << "UEVar:\t" << *(*it) << "\n";
+                        errs() << "UEVar:\t" << *(*it) << "\n";
+                        // has this variable already in UEE
                         UEEIt = UEE->begin();
                         if (std::find(UEE->begin(),UEE->end(),*var)==UEE->end()){
                             UEE->insert(UEEIt, *var);
@@ -84,7 +98,7 @@ struct Liveness : public FunctionPass {
             }
             std::string *op3_str = new std::string;
             *op3_str = op3->getName();
-//            errs() << "op3:\t" << *op3_str << "\n";
+           errs() << "KillSet:\t" << *op3_str << "\n";
             killSetIt = killSet->begin();
             killSet->insert(killSetIt, *op3_str);
           }
@@ -104,9 +118,11 @@ struct Liveness : public FunctionPass {
         std::vector<std::string> *liveB;
         auto search = liveOut.find(basic_block);
         if ( search != liveOut.end()){
+            // if liveout is already stored
             liveB = search->second;
         }
         else {
+            // it is a new BB that haven't store in Liveout
             liveB = new std::vector<std::string>;
         }
         // Update the Liveout of this block based on its successors
@@ -120,23 +136,30 @@ struct Liveness : public FunctionPass {
             std::vector<std::string> diff;
             std::map<BasicBlock*, std::vector<std::string>*>::iterator searchS;
 
+            // find the killset of this BB
             searchS = killSet.find(Succ);
             if (searchS != killSet.end()){
                 kSucc = searchS->second;
             }
             else {
-                errs() << "!!!!! the block " << Succ <<" is not in Killset list !!!!!\n";
+                // errs() << "!!!!! the block " << Succ <<" is not in Killset list !!!!!\n";
                 return false;
             }
+
+            // find the UUE of this BB
             searchS = UEE.find(Succ);
             if (searchS != UEE.end()){
                 uSucc = searchS->second;
             }
             else {
-                errs() << "!!!!! the block is "<< Succ << "not in UEE list !!!!!\n";
+                // errs() << "!!!!! the block is "<< Succ << "not in UEE list !!!!!\n";
                 return false;
             }
+
+            // find the liveOut of this BB
             searchS = liveOut.find(Succ);
+
+            // do the formula: Liveout(successor) - Killset(successor)
             if (searchS != liveOut.end()){
                 lSucc = searchS->second;
                 setDiff(lSucc,kSucc,&diff);
@@ -149,22 +172,24 @@ struct Liveness : public FunctionPass {
 //            printVarList("diff",&diff);
 //            printVarList("uSucc",uSucc);
 
+            // do the formula: ... + UEE(successor)
             setUnion(&diff,uSucc,tmp2);
 
             auto tmp3 = new std::vector<std::string>;
-
+            // union this successor with liveB and store in tmp3
             setUnion(tmp2,tempLiveB,tmp3);
 
 //            printVarList("tmp2",tmp2);
 //            printVarList("tempLiveB",tempLiveB);
 //            printVarList("tmp3",tmp3);
-
+            // update liveB for this successor
             tempLiveB = tmp3;
 
 //            printVarList("tempLiveB after",tempLiveB);
 
 
-        }
+        } // end of successor iteration
+        // check for liveout change: basic iteration algorithm
         bool changed = false;
         if (tempLiveB->size()!=liveB->size() || search==liveOut.end()){
             changed =true;
@@ -196,19 +221,22 @@ struct Liveness : public FunctionPass {
     }
 
     void printVarList(std::string listName, std::vector<std::string> *tmp){
-        errs() << listName <<":\n";
+        // errs() << listName <<":\n";
         for (auto i1=tmp->begin();i1!=tmp->end();i1++){
-            errs() << " "<< *i1 << "\n";
+            // errs() << " "<< *i1 << "\n";
         }
     }
 
+    // back to front traverse, return the order how bb should be visited
     std::list<BasicBlock*>* PostOrderTraverse(BasicBlock* bb, std::list<BasicBlock*> *visited, std::list<BasicBlock*> *order) {
         visited->push_back(bb);
         for (BasicBlock *succ : successors(bb)) { // Parse the left tree and, then the right tree. Technically, it would be recursive call on every successor
             if (std::find(visited->begin(), visited->end(), succ)==visited->end()){
+                //if the successor hasn't been visited before, keep on searching on its successors
                 visited = PostOrderTraverse(succ,visited,order);
             }
         }
+        // when it come the bb which has no successor, put in the order list and return to is ancestoer saying I was visited,is your turn now !
         order->push_back(bb);
         return visited;
     }
@@ -238,7 +266,7 @@ struct Liveness : public FunctionPass {
     }
 
     bool static compareStrings(std::string str1, std::string str2){
-        errs() << "str1:" << str1 << "\tstr2:" << str2 << "\tcmp:" <<std::to_string(str1.compare(str2)) << "\n";
+        // errs() << "str1:" << str1 << "\tstr2:" << str2 << "\tcmp:" <<std::to_string(str1.compare(str2)) << "\n";
         return str1.compare(str2)==0;
     }
 
@@ -261,17 +289,17 @@ struct Liveness : public FunctionPass {
     }
 
     void printTraverseOrder( std::list<BasicBlock*> *visitedBlocks,  std::list<BasicBlock*> *order){
-        errs() << "visited:\n";
+        // errs() << "visited:\n";
         for(auto itt = visitedBlocks->begin(); itt!=visitedBlocks->end();itt++){
-            errs() << *itt << ", ";
+            // errs() << *itt << ", ";
         }
-        errs() << "\n";
+        // errs() << "\n";
 
-        errs() << "PO:\n";
+        // errs() << "PO:\n";
         for(auto itt = order->begin(); itt!=order->end();itt++){
-            errs() << *itt << ", ";
+            // errs() << *itt << ", ";
         }
-        errs() << "\n";
+        // errs() << "\n";
     }
 
   bool runOnFunction(Function &F) override {
@@ -279,7 +307,7 @@ struct Liveness : public FunctionPass {
             return false;
         }
         else{
-            errs() << "-----------Function name: " << F.getName() << "-----------\n";
+            // errs() << "-----------Function name: " << F.getName() << "-----------\n";
         }
       killSet.clear();
       UEE.clear();
@@ -292,6 +320,7 @@ struct Liveness : public FunctionPass {
       }
       BasicBlock &basicBlock = F.getEntryBlock();
       std::list<BasicBlock*> *visited = new std::list<BasicBlock*>;
+      // get the traverse function
       auto visitedBlocks = PostOrderTraverse(&basicBlock,visited,&worklist);
 
       //printTraverseOrder(visited, &worklist);
